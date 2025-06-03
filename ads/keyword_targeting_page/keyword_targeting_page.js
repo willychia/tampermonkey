4bf54d4a65fec3694066acf9b21f474400bfbb95
willychia <157714048+willychia@users.noreply.github.com>
2025-06-03 13:20:37 +0800

    Update keyword_targeting_page.js

--- ads/keyword_targeting_page/keyword_targeting_page.js
+++ ads/keyword_targeting_page/keyword_targeting_page.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Keyword Targeting Page Additional Function
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-29.2
+// @version      2025-06-03
 // @description  Add enhanced features to Tabulator table, using tabulator
 // @author       Willy Chia
 // @match        https://admin.hourloop.com/amazon_ads/sp/keywords?*
